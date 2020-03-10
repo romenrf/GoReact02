be996436e3d99b4e2e2d9c2e888f9816a8a96743
Romerf <romerf@gmail.com>
2020-03-10 13:32:20 +0000

    Cambios en Websocket

--- backend/websocket/websocket.go
+++ backend/websocket/websocket.go
@@ -15,6 +15,7 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
+//TEST
 func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
